ff764e43f97090ead43dd52df0a9e27ca4b0214f
aditeya <22963960+Aditeya@users.noreply.github.com>
2024-08-28 16:47:18 +0530

    docs: ✏️cast file w/ mkgif script

---
+++ casts/mkgif.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+agg_present() {
+	agg -v --theme asciinema --font-family "Iosevka Term Slab,Apple Color Emoji" --font-size 16 --speed 2 $1 $2
+}
+
+agg_present hi.cast hi.gif &
+agg_present demo1.cast demo1.gif &
+agg_present demo2.cast demo2.gif &
+
+wait
